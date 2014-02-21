ef67139097dd9027b1352d48a955ef52fcb4b123
Evan Hahn <me@evanhahn.com>
2014-02-21 13:15:20 -0500

    if you're logged in, don't show the register form

--- js/the.js
+++ js/the.js
@@ -1,3 +1,5 @@
+// jshint undef: false
+
 Parse.initialize('4SV3X5Flt3tqhr87pM29xI36jKYtUWnZWBBI70iH', 'CuVq6V6rVsXC2ud1lGl9U8qStG2zVNySqPktbk35');
 
 var $activities = $('.activity');
@@ -12,17 +14,19 @@ $registerForm.on('submit', function(event) {
 
 	Parse.User.logIn(username, password, {
 		success: function(user) {
-
 			$activities.hide();
 			$gameList.show();
-
 		},
-		error: function (user, error) {
-			// body...
+		error: function(user, error) {
 			alert('You did not log in successfully');
 		}
 	});
 
 });
 
-$registerForm.show();
+var currentUser = Parse.User.current();
+if (currentUser) {
+	$gameList.show();
+} else {
+	$registerForm.show();
+}
