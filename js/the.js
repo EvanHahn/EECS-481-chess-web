a80520ecfae79370fd6172b41e7fdf9c3b461673
Evan Hahn <me@evanhahn.com>
2014-02-21 17:35:41 -0500

    clean up the code just a bit, add log out

--- js/the.js
+++ js/the.js
@@ -4,29 +4,54 @@ Parse.initialize('4SV3X5Flt3tqhr87pM29xI36jKYtUWnZWBBI70iH', 'CuVq6V6rVsXC2ud1lG
 
 var $activities = $('.activity');
 var $registerForm = $('#register-form');
+var $registerFormAlert = $('.alert', $registerForm);
 var $gameList = $('#game-list');
+var $logOut = $('#log-out');
+
+function showRegister() {
+	console.assert(!Parse.User.current());
+	$activities.hide();
+	$logOut.hide();
+	$registerFormAlert.hide();
+	$registerForm.show();
+}
+
+function showGameList(user) {
+	$activities.hide();
+	$logOut.show();
+	$gameList.show();
+}
 
 $registerForm.on('submit', function(event) {
 
 	event.preventDefault();
-	var username = $('#user-name').val();
-	var password = $('#password').val();
+
+	var form = $(this).serializeObject();
+	var username = form.username;
+	var password = form.password;
 
 	Parse.User.logIn(username, password, {
 		success: function(user) {
-			$activities.hide();
-			$gameList.show();
+			showGameList(user);
 		},
-		error: function(user, error) {
-			alert('You did not log in successfully');
+		error: function() {
+			$registerFormAlert.show();
 		}
 	});
 
 });
 
-var currentUser = Parse.User.current();
-if (currentUser) {
-	$gameList.show();
-} else {
-	$registerForm.show();
-}
+$logOut.on('click', function(event) {
+	event.preventDefault();
+	Parse.User.logOut();
+	showRegister();
+});
+
+$(document).on('ready', function() {
+	var currentUser = Parse.User.current();
+	if (currentUser)
+		showGameList(currentUser);
+	else
+		showRegister();
+});
+
